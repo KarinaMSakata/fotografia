8088f141e76a801894803b70cc1fba31ef04faff
Karina Sakata <karina.msakata@hotmail.com>
2023-01-25 17:26:35 -0300

    criação de nav

---
+++ spec/system/user_view_homepage_spec.rb
@@ -0,0 +1,13 @@
+require  'rails_helper'
+
+describe 'Usuário visita tela inicial' do
+  it 'e vê o nome da app' do
+    visit root_path
+    
+    within 'nav' do
+      expect(page).to have_content('Galeria')
+      expect(page).to have_content('Vem me conhecer')
+      expect(page).to have_content('Vamos conversar?')
+    end
+  end 
+end
