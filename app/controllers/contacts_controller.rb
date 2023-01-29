31d37ac370b6bc2ab770c7323f6fb0c78fab0443
Karina Sakata <karina.msakata@hotmail.com>
2023-01-28 22:25:34 -0300

    formulario de mensagem

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,12 @@
+class ContactsController < ApplicationController
+  
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: "home#index"
   resources :summary, only: [:index]
+  resources :contacts, only: [:index, :new, :create]
 end
